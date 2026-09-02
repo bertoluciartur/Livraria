e2d2cfd861ffb1df048b162ecad698f1e32c5098
Artur Bertoluci <bertoluciartur@gmail.com>
2026-09-02 15:36:32 -0300

    Remover pasta duplicada e organizar estrutura

--- Livraria - Rocketseat/src/main/java/Main.java
+++
@@ -1,7 +0,0 @@
-import livraria.services.GerenciarLivraria;
-
-public class Main {
-    public static void main(String args[]){
-        GerenciarLivraria gerenciarLivraria = new GerenciarLivraria();
-    }
-}
